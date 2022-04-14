e02568deaa3b0be66967b3062407573f01c7004a
paccydev <paccydev@gmail.com>
2022-04-15 01:42:13 +0200

    very first commit

--- prisonmis/settings.py
+++ prisonmis/settings.py
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'prisonmis.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'prisonmis',
-        'USER': 'postgres',
-        'PASSWORD': 'User1245@',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'prisonmis',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'User1245@',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
 
     }
 }
